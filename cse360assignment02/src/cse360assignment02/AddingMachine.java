572524ab14c39b8e8f18436b04b074fe8d3362d6
JuanCarlos3 <hwankim2k@gmail.com>
2020-10-05 19:22:25 -0700

    Initial Version

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,51 @@
+package cse360assignment02;
+
+/**
+ * Machine class
+ * Has a value total
+ */
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * Constructor Method
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Gets the total
+   * @return the total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Adds given value to total
+   * @param value: the value being added
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * subtracts given value to total
+   * @param value: the value being subtracted
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * prints out given information & transaction
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * clears values in "memory"
+   */
+  public void clear() {
+  }
+}
